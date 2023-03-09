977ab13db2dcd418f3975ebf9ab7f5d4dc0fa74f
MillyLu <mji23@mail.ru>
2023-03-09 21:36:14 +0300

    hw5.10tests

--- src/components/bar-volumeProgress/volumeProgress.jsx
+++ src/components/bar-volumeProgress/volumeProgress.jsx
@@ -4,7 +4,7 @@ import * as Styled from './styles';
 export function VolumeProgress({volume, setVolume}){
     return(
         <Styled.VolumeProgress>
-            <Styled.VolumeProgressLine value={volume} name='range' min={0} max={100} onChange={(e) => setVolume(e.target.value)} />
+            <Styled.VolumeProgressLine data-testid="progress" value={volume} name='range' min={0} max={100} onChange={(e) => setVolume(e.target.value)} />
         </Styled.VolumeProgress>
     )
 }
